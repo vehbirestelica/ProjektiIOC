01e3e10c6c2ea32adac24b27afcd980ccb3f5689
vetonshala <vetonnshala@gmail.com>
2020-06-14 10:28:43 +0200

    Feat: login & register activity

--- app/src/main/java/com/fiek/projektiioc/LoginActivity.java
+++ app/src/main/java/com/fiek/projektiioc/LoginActivity.java
@@ -2,41 +2,30 @@ package com.fiek.projektiioc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-   Button loginbutton;
-   TextView txtRegjistrohubtn;
-
+public class LoginActivity extends AppCompatActivity {
+      Button btnKyqu;
+      TextView txtRegjistrohubtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        loginbutton = findViewById(R.id.btnKyqu);
-        loginbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_login);
 
         txtRegjistrohubtn = (TextView)findViewById(R.id.txtRegjistrohu);
         txtRegjistrohubtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(intent);
             }
         });
+
 
 
 
---
+++ app/src/main/java/com/fiek/projektiioc/RegisterActivity.java
@@ -0,0 +1,30 @@
+package com.fiek.projektiioc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+Button txtRegjistrohu;
+TextView txtKyqubtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+
+        txtKyqubtn = (TextView)findViewById(R.id.txtKyqu);
+        txtKyqubtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
