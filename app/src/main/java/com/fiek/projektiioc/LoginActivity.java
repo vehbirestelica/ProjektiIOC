ff2d0e4e6a46c25321070d5cc6f68693d6cc0409
Arton Thaqi <artonth@outlook.com>
2020-06-15 12:38:09 +0200

    Login & Register authectitacion using Firebase

--- app/src/main/java/com/fiek/projektiioc/LoginActivity.java
+++ app/src/main/java/com/fiek/projektiioc/LoginActivity.java
@@ -1,22 +1,79 @@
 package com.fiek.projektiioc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-      Button btnKyqu;
-      TextView txtRegjistrohubtn;
+
+    EditText lEmail,lPassword;
+    Button btnLkyqu;
+    TextView txtRegjistrohubtn;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        txtRegjistrohubtn = (TextView)findViewById(R.id.txtRegjistrohu);
+
+        lEmail =  findViewById(R.id.txtLEmail);
+        lPassword = findViewById(R.id.txtLPassword);
+        txtRegjistrohubtn = findViewById(R.id.txtRegjistrohu);
+        progressBar =  findViewById(R.id.LprogressBar);
+        fAuth = FirebaseAuth.getInstance();
+        btnLkyqu = findViewById(R.id.btnLkyqu);
+
+        btnLkyqu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = lEmail.getText().toString().trim();
+                String password = lPassword.getText().toString().trim();
+
+                if (email.isEmpty()){
+                    lEmail.setError("Emaili duhet te vendoset");
+                    return;
+                }
+                if (password.isEmpty()){
+                    lPassword.setError("Passwordi duhet te vendoset");
+                    return;
+                }
+                if (password.length()<6){
+                    lPassword.setError("Passwordi duhet te permbaj sepaku 6 karaktere");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //autentikimi
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Logged in Successfully",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this,"Error "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
         txtRegjistrohubtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/fiek/projektiioc/RegisterActivity.java
+++ app/src/main/java/com/fiek/projektiioc/RegisterActivity.java
@@ -1,22 +1,91 @@
 package com.fiek.projektiioc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-Button txtRegjistrohu;
-TextView txtKyqubtn;
+
+    Button btnRegjistrohu;
+    TextView txtKyqubtn;
+    EditText rName,rEmail,rCompaniID,rPassword;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        txtKyqubtn = (TextView)findViewById(R.id.txtKyqu);
+        rName = findViewById(R.id.txtemri);
+        rEmail = findViewById(R.id.txtemail);
+        rCompaniID = findViewById(R.id.txtkompaniaid);
+        rPassword = findViewById(R.id.txtpassword);
+        txtKyqubtn = findViewById(R.id.txtKyqu);
+        btnRegjistrohu =  findViewById(R.id.btnRegjistrohu);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        // Nese useri ekziston
+//        if(fAuth.getCurrentUser() != null){
+//
+//        }
+
+        btnRegjistrohu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = rEmail.getText().toString().trim();
+                String password = rPassword.getText().toString().trim();
+
+                if (email.isEmpty()){
+                    rEmail.setError("Emaili duhet te vendoset");
+                    return;
+                }
+                if (password.isEmpty()){
+                    rPassword.setError("Passwordi duhet te vendoset");
+                    return;
+                }
+                if (password.length()<6){
+                    rPassword.setError("Passwordi duhet te permbaj sepaku 6 karaktere");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // Regjistrimi
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this,"User Created",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(RegisterActivity.this,"Error "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
         txtKyqubtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
