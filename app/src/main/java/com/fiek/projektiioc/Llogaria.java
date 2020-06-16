f5400b68126f03a172db1af6a3bcd7909caed6c1
vetonshala <vetonnshala@gmail.com>
2020-06-16 19:28:41 +0200

    Small fixes

---
+++ app/src/main/java/com/fiek/projektiioc/Llogaria.java
@@ -0,0 +1,14 @@
+package com.fiek.projektiioc;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Llogaria extends Fragment {
+
+
+}
