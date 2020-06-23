3a694efb3dba978de1587db3692126d9cd83cbe7
Arton Thaqi <artaan.thaqi@gmail.com>
2020-06-24 00:18:12 +0200

    github email changed

--- app/src/main/java/com/fiek/projektiioc/Invoice_details.java
+++ app/src/main/java/com/fiek/projektiioc/Invoice_details.java
@@ -24,7 +24,6 @@ public class Invoice_details extends AppCompatActivity {
         Invoice invoice = intent.getParcelableExtra("invoice");
 
 
-
         title = findViewById(R.id.inv_det_title);
         type = findViewById(R.id.inv_det_type);
         sum = findViewById(R.id.inv_det_sum);
