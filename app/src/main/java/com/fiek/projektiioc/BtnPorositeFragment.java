fed53dc05ddad5de47b21e1cad6bc2b4744b0e51
Vehbi Restelica <vehbi.res@gmail.com>
2020-06-26 17:26:32 +0200

    Orders ListView access denied for non-administrators

--- app/src/main/java/com/fiek/projektiioc/BtnPorositeFragment.java
+++ app/src/main/java/com/fiek/projektiioc/BtnPorositeFragment.java
@@ -2,6 +2,7 @@ package com.fiek.projektiioc;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +16,7 @@ import androidx.fragment.app.Fragment;
 public class BtnPorositeFragment extends Fragment {
     Button btnOrderCompleted;
     Button btnOrderProcessing;
-    Button getBtnOrderNotPaid;
+    Button btnOrderNotPaid;
     Button addNewOrder;
     Button btnnewproducts;
     Button btnMyOrders;
@@ -24,6 +25,12 @@ public class BtnPorositeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_btn_porosite, container, false);
+        int companyID = 0;
+        Bundle bundle = this.getArguments();
+        if (bundle != null){
+            companyID = bundle.getInt("Company ID:", 19999);
+        }
+        Log.d("companyid", "onCreateView: " + companyID);
 
         addNewOrder = (Button) view.findViewById(R.id.btn_newOrder);
         addNewOrder.setOnClickListener(new View.OnClickListener(){
@@ -34,6 +41,11 @@ public class BtnPorositeFragment extends Fragment {
         });
 
         btnOrderCompleted = (Button) view.findViewById(R.id.btn_orderCompleted);
+        if(companyID>10000 && companyID<20000){
+            btnOrderCompleted.setVisibility(View.VISIBLE);
+        } else {
+            btnOrderCompleted.setVisibility(View.GONE);
+        }
         btnOrderCompleted.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(),OrdersActivity.class);
@@ -42,6 +54,11 @@ public class BtnPorositeFragment extends Fragment {
         });
 
         btnOrderProcessing = (Button) view.findViewById(R.id.btn_orderOnProces);
+        if(companyID>10000 && companyID<20000){
+            btnOrderProcessing.setVisibility(View.VISIBLE);
+        } else {
+            btnOrderProcessing.setVisibility(View.GONE);
+        }
         btnOrderProcessing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
@@ -50,8 +67,13 @@ public class BtnPorositeFragment extends Fragment {
             }
         });
 
-        getBtnOrderNotPaid = (Button) view.findViewById(R.id.btn_orderCancel2);
-        getBtnOrderNotPaid.setOnClickListener(new View.OnClickListener() {
+        btnOrderNotPaid = (Button) view.findViewById(R.id.btn_orderCancel2);
+        if(companyID>10000 && companyID<20000){
+            btnOrderNotPaid.setVisibility(View.VISIBLE);
+        } else {
+            btnOrderNotPaid.setVisibility(View.GONE);
+        }
+        btnOrderNotPaid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
                 Intent intent3 = new Intent(getActivity(),NotPaidOrders.class);
