c5b0c252b265fa7abe429ddd0059aa0247ddc5a4
Vehbi Restelica <vehbi.res@gmail.com>
2020-07-02 20:12:30 +0200

    Multiple Screen Adjustment

--- app/src/main/java/com/fiek/projektiioc/Porosite/MyOrders.java
+++ app/src/main/java/com/fiek/projektiioc/Porosite/MyOrders.java
@@ -1,26 +1,20 @@
 package com.fiek.projektiioc.Porosite;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.fiek.projektiioc.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,8 +23,7 @@ public class MyOrders extends AppCompatActivity {
     private FirebaseDatabase mDatabase;
     private DatabaseReference myRef;
     private List<NewOrdersActivity> orders = new ArrayList<>();
-    private ArrayList<String> arrayList = new ArrayList<String>();
-    String value;
+    List<NewOrders> arrayList;
     private ListView mListView;
 
     @Override
@@ -39,57 +32,30 @@ public class MyOrders extends AppCompatActivity {
         setContentView(R.layout.activity_orders);
 
         mListView = (ListView) findViewById(R.id.listView);
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
-        mListView.setAdapter(arrayAdapter);
+        arrayList = new ArrayList<>();
 
+    }
+    @Override
+    protected void onStart () {
+        super.onStart();
         FirebaseUser auth;
         auth = FirebaseAuth.getInstance().getCurrentUser();
         String currentUser = auth.getUid();
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String currentItem = parent.getItemAtPosition(position).toString();
-                Intent onClickintent = new Intent(MyOrders.this, ListViewOnClickListener.class);
-                onClickintent.putExtra("myorders",currentItem);
-                startActivity(onClickintent);
-
-                Toast.makeText(MyOrders.this,"dwdwddwdwd",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Query query  = FirebaseDatabase.getInstance().getReference("addOrder")
+        Query query = FirebaseDatabase.getInstance().getReference("addOrder")
                 .orderByChild("userID")
-                .equalTo(currentUser);
-        query.addChildEventListener(new ChildEventListener() {
+                .equalTo(currentUser);;
+        query.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChildAdded (@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                String emri = snapshot.child("derguesi").getValue(String.class);
-                String data = snapshot.child("marresi").getValue(String.class);
-                String statusi = snapshot.child("statusi").getValue(String.class);
-                String ordersID = snapshot.getKey();
-                String value = ordersID + " \t\t\t\t\t " + data + " \t\t\t\t" + statusi;
-                NewOrders newOrders = new NewOrders();
-                arrayList.add(value);
-//                arrayList.add(data);
-//                arrayList.add(statusi);
-                arrayAdapter.notifyDataSetChanged();
-            }
+            public void onDataChange (@NonNull DataSnapshot snapshot) {
+                arrayList.clear();
+                for(DataSnapshot ordersSnapshot : snapshot.getChildren()){
+                    NewOrders orders = ordersSnapshot.getValue(NewOrders.class);
 
-            @Override
-            public void onChildChanged (@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                    arrayList.add(orders);
+                }
 
-            }
-
-            @Override
-            public void onChildRemoved (@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved (@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
+                MyOrdersAdapter adapter = new MyOrdersAdapter(MyOrders.this, arrayList);
+                mListView.setAdapter(adapter);
             }
 
             @Override
@@ -97,6 +63,5 @@ public class MyOrders extends AppCompatActivity {
 
             }
         });
-
     }
 }
